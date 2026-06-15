c523f9f8529773c4550d9cdba02b0130863787c7
CarlosMen101 <cmendeze_24_2@unc.edu.pe>
2026-06-14 22:49:32 -0500

    Ejercicio propuesto solucionado

--- WpfApp1/Ejercicio_Propuesto/MainWindow.xaml.cs
+++ WpfApp1/Ejercicio_Propuesto/MainWindow.xaml.cs
@@ -20,5 +20,42 @@ namespace Ejercicio_Propuesto
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            double monto = 0 ;
+            if (double.TryParse(tbxIngresos.Text, out monto) != true) {
+
+                MessageBox.Show("Ingrese un numero");
+            }
+            ;
+            double fonaviMonto = ((monto / 100) * 8);
+            double importeMonto = ((monto / 100) * 5);
+            double AFPMonto = ((monto / 100) * 12);
+            
+            if (cbFonavi.IsChecked == true) {
+
+                monto = monto - fonaviMonto;
+                lbFonavi.Content = ("Fonavi: "+ fonaviMonto );
+
+                lbTotal.Content = ("Total a pagar: "+monto); 
+            }
+            if (cbImporte1.IsChecked == true)
+            {
+                monto = monto - importeMonto;
+                lbImporte.Content = ("Importe: " + importeMonto);
+                lbTotal.Content = ("Total a pagar: " + monto);
+            }
+            if (cbAFP.IsChecked == true)
+            {
+                monto = monto - AFPMonto;
+                lbAFP.Content = ("AFP: " + AFPMonto);
+                lbTotal.Content = ("Total a pagar: " + monto);
+            }
+            if (cbAFP.IsChecked != true && cbImporte1.IsChecked != true && cbFonavi.IsChecked != true) {
+
+                lbTotal.Content = ("Total a pagar: " + monto);
+            }
+        }
     }
 }
