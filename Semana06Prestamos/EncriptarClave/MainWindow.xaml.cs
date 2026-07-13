c10916b6a91890b2efa51a53347f5ad03bca4dcc
CarlosMen101 <cmendeze_24_2@unc.edu.pe>
2026-07-13 07:53:03 -0500

    Semana 07 Adivina Edad Diseño UI

--- Semana06Prestamos/EncriptarClave/MainWindow.xaml.cs
+++ Semana06Prestamos/EncriptarClave/MainWindow.xaml.cs
@@ -20,5 +20,46 @@ namespace EncriptarClave
         {
             InitializeComponent();
         }
+
+        private void btnEncriptar_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbxClave.Text))
+            {
+                MessageBox.Show("Por favor, ingrese una clave para encriptar.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            string claveEncriptada = EncriptarClave(tbxClave.Text);
+
+            tbxResultado.Text = claveEncriptada;
+        }
+        private string EncriptarClave(string texto)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            foreach (char caracter in texto)
+            {
+                // Encriptar letras mayúsculas
+                if (char.IsUpper(caracter))
+                {
+                    char c = (char)(((caracter + 3 - 'A') % 26) + 'A');
+                    resultado.Append(c);
+                }
+                // Encriptar letras minúsculas
+                else if (char.IsLower(caracter))
+                {
+                    char c = (char)(((caracter + 3 - 'a') % 26) + 'a');
+                    resultado.Append(c);
+                }
+                // Si es un número, espacio o signo, se queda igual
+                else
+                {
+                    resultado.Append(caracter);
+                }
+            }
+
+            return resultado.ToString();
+        }
+
     }
+
 }
